9625fd4a7c4db112f962c5adc9a4aa4a93218f68
Valdemir Bezerra de Souza Junior <52362288+ValdemirBSJr@users.noreply.github.com>
2023-12-20 10:22:33 -0300

    Update 03_fix_start.py

--- 03_fix_start.py
+++ 03_fix_start.py
@@ -1,21 +1,29 @@
 """
-04. mix_up
+03. fix_start
 
-Dadas as strings a e b, retorne uma string com a e b separados
-por um espaço '<a> <b>', além disso, troque os 2 primeiros caracteres
-das duas strings.
+Dada uma string s, retorne uma string onde
+todas as ocorrências do primeiro caracter de s
+foram substituidas por '*', exceto a primeira.
 
-Exemplo:
-    'mix', 'pod' -> 'pox mid'
-    'dog, 'dinner' -> 'dig donner'
+Exemplo: 'babble' retorna 'ba**le'
 
-Assuma que a e b tem tamanho 2 ou maior.
+Assuma que a string tem tamanho 1 ou maior.
+
+Dica: s.replace(stra, strb) retorna uma versão da string s
+onde todas as instancias de stra foram substituidas por strb.
 """
 
-def mix_up(a, b):
-    string_1 = b[0:2] + a[2:]
-    string_2 = a[0:2] + b[2:]
-    return f'{string_1} {string_2}'
+def fix_start(s):
+    '''
+    Pega uma string passada e ve qual a primeira letra
+    em seguida ve se ela se repete na string, caso se repita, altera para *
+    pula primeira letra para que ele nao a substitua
+    por fim join pra juntar todas as letras
+    :param s: string de entrada
+    :return: retorna a string concatenada a primeira letra da string com o restante dela
+    '''
+    string_alterada = ''.join([caractere if caractere not in s[0] else '*' for caractere in s[1:]])
+    return f'{s[0]}{string_alterada}'
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
@@ -25,7 +33,7 @@ def test(f, in_, expected):
     Executa a função f com o parâmetro in_ e compara o resultado com expected.
     :return: Exibe uma mensagem indicando se a função f está correta ou não.
     """
-    out = f(*in_)
+    out = f(in_)
 
     if out == expected:
         sign = '✅'
@@ -34,12 +42,12 @@ def test(f, in_, expected):
         sign = '❌'
         info = f'e o correto é {expected!r}'
 
-    print(f'{sign} {f.__name__}{in_!r} retornou {out!r} {info}')
+    print(f'{sign} {f.__name__}({in_!r}) retornou {out!r} {info}')
 
 
 if __name__ == '__main__':
     # Testes que verificam o resultado do seu código em alguns cenários.
-    test(mix_up, ('mix', 'pod'), 'pox mid')
-    test(mix_up, ('dog', 'dinner'), 'dig donner')
-    test(mix_up, ('gnash', 'sport'), 'spash gnort')
-    test(mix_up, ('pezzy', 'firm'), 'fizzy perm')
+    test(fix_start, 'babble', 'ba**le')
+    test(fix_start, 'aardvark', 'a*rdv*rk')
+    test(fix_start, 'google', 'goo*le')
+    test(fix_start, 'donut', 'donut')
